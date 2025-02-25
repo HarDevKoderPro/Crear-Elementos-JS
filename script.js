4ec6a776b202a4153424d62c68245b552c3c18f0
HardevKoder <hardevkoder@gmail.com>
2025-02-25 00:15:16 -0500

    Ajustes Finales

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 "use strict";
-import { Libreria } from "./libreria";
+import { Libreria } from "./libreria.js";
 // -------------------------------------------
 // CREACION DE ELEMENTOS
 // -------------------------------------------
